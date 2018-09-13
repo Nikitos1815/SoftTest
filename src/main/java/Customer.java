2af1cda392adfa964446eedcd248eccbc288a32d
Nikita Harzha <chessmasters277@gmail.com>
2018-09-13 22:45:54 +0300

    Improved code

---
+++ src/main/java/Customer.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Customer {
+    private String name;
+
+    public Customer(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(){
+        this.name = name;
+    }
+}
---
+++ src/main/java/Rental.java
@@ -0,0 +1,47 @@
+import java.util.*;
+public class Rental {
+    private int kind;
+    private int days;
+
+
+    public Rental(int kind, int days){
+        this.kind = kind;
+        this.days = days;
+    }
+    public int getKind(){
+        return kind;
+    }
+    public void setKind(int kind){
+        this.kind = kind;
+    }
+    public void setDays(int days){
+        this.days = days;
+    }
+    public int getDays(){
+        return days;
+    }
+    public double getBasePrice(){
+        //TODO Some program code
+        return days + kind;
+    }
+    private double amountFor(Rental rental){
+        double result;
+        int choice;
+        result = rental.getDays()*rental.getBasePrice();
+        choice = rental.getKind();
+        switch (choice){
+            case 1:
+                result = result*1.5;
+            case 2:
+                result = result*2;
+            case 3:
+                result = result*2.5;
+
+        }
+        if(rental.getDays() > 7)
+            result = result*3;
+
+        return result;
+
+    }
+}
